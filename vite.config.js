182c8db655070eed8fac32e6afa1e0ed79a760f7
Zahid Ali <k24sw012zahid2645khan873@gmail.com>
2025-11-19 14:12:36 +0500

    Change base path in Vite config for Vercel
    Updated base path for Vercel deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ZahidKhan_portfolio/',  // <-- make sure this is exactly like this
   plugins: [react()],
-})
+  base: '/',   // 👈 IMPORTANT FOR VERCEL
+});
+
